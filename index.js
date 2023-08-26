ec476d6df6c26c5b3fccc5dd43b79c80ee0f1c83
shubham <shubham@shubhams-MacBook-Air.local>
2023-08-27 02:07:34 +0530

    love vercel1

--- index.js
+++ index.js
@@ -2,23 +2,466 @@ const express = require('express');
 const fs = require('fs').promises;
 
 const app = express();
-
+const data={
+    "response_code": 0,
+    "results": [
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What does 'SQL' stand for in the context of databases?",
+            "correct_answer": "Structured Query Language",
+            "incorrect_answers": [
+                "System Query Language",
+                "Simple Query Language",
+                "Standard Query Language"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which term refers to a collection of related data entries in a database?",
+            "correct_answer": "Table",
+            "incorrect_answers": [
+                "Row",
+                "Column",
+                "Database"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What type of key uniquely identifies a record within a database?",
+            "correct_answer": "Primary Key",
+            "incorrect_answers": [
+                "Foreign Key",
+                "Candidate Key",
+                "Composite Key"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which SQL command is used to retrieve data from a database?",
+            "correct_answer": "SELECT",
+            "incorrect_answers": [
+                "UPDATE",
+                "DELETE",
+                "INSERT"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which type of database model represents data as a collection of tables?",
+            "correct_answer": "Relational Model",
+            "incorrect_answers": [
+                "Hierarchical Model",
+                "Network Model",
+                "Entity-Relationship Model"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What is the purpose of the 'JOIN' operation in SQL?",
+            "correct_answer": "To combine data from two or more tables",
+            "incorrect_answers": [
+                "To create a new table",
+                "To delete data",
+                "To update data"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which SQL command is used to add new records to a database?",
+            "correct_answer": "INSERT",
+            "incorrect_answers": [
+                "UPDATE",
+                "DELETE",
+                "SELECT"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "In a relational database, what is the relationship between two tables through a common column?",
+            "correct_answer": "Foreign Key",
+            "incorrect_answers": [
+                "Primary Key",
+                "Composite Key",
+                "Unique Key"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which term refers to the process of improving the design of a database schema?",
+            "correct_answer": "Normalization",
+            "incorrect_answers": [
+                "Denormalization",
+                "Optimization",
+                "Refactoring"
+            ]
+        },
+        {
+            "category": "DBMS",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which database model stores data in a tree-like structure?",
+            "correct_answer": "Hierarchical Model",
+            "incorrect_answers": [
+                "Relational Model",
+                "Network Model",
+                "Entity-Relationship Model"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "What is the primary function of an operating system?",
+            "correct_answer": "Manage hardware and software resources",
+            "incorrect_answers": [
+                "Run applications",
+                "Provide internet connectivity",
+                "Display graphical user interfaces"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Multitasking is the ability of an operating system to execute multiple processes concurrently.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False",
+                "IDK",
+                "NON"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "The process of swapping data in and out of RAM to the storage is called ____________.",
+            "correct_answer": "paging",
+            "incorrect_answers": [
+                "imaging",
+                "webing",
+                "stunning+"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which file system is commonly used in Linux distributions?",
+            "correct_answer": "Ext4",
+            "incorrect_answers": [
+                "NTFS",
+                "FAT32",
+                "HFS+"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which of the following scheduling algorithms can cause starvation?",
+            "correct_answers": [
+                "First-Come, First-Served",
+                "Priority Scheduling"
+            ],
+            "incorrect_answers": [
+                "Round Robin",
+                "Shortest Job Next"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which synchronization primitive is used to ensure that only one thread can access a critical section at a time?",
+            "correct_answer": "Mutex",
+            "incorrect_answers": [
+                "Semaphore",
+                "Barrier",
+                "Spinlock"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Virtual memory allows programs to use more memory than is physically available.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False",
+                "IDK",
+                "NON"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "The first program that runs when a computer starts is called the ________.",
+            "correct_answer": "bootloader",
+            "incorrect_answers": [
+                "imaging",
+                "webing",
+                "stunning+"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which permissions can be assigned to a file in Unix-like operating systems?",
+            "correct_answers": [
+                "Read",
+                "Write",
+                "Execute"
+            ],
+            "incorrect_answers": [
+                "Edit",
+                "Delete",
+                "Modify"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which of the following conditions is necessary for a deadlock to occur?",
+            "correct_answer": "Mutual Exclusion",
+            "incorrect_answers": [
+                "Preemption",
+                "Circular Wait",
+                "No Resource Holding"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which of the following is NOT a valid process state?",
+            "correct_answer": "Sleeping",
+            "incorrect_answers": [
+                "Running",
+                "Blocked",
+                "Ready"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "The kernel is a separate program that provides an interface for user applications to interact with hardware.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False",
+                "IDK",
+                "NON"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "The command-line interface of an operating system is often referred to as the ________.",
+            "correct_answer": "shell",
+            "incorrect_answers": [
+                "Kernel",
+                "Prompt",
+                "Alert"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which of the following are levels in the memory hierarchy?",
+            "correct_answers": [
+                "Cache",
+                "Main Memory",
+                "Secondary Storage"
+            ],
+            "incorrect_answers": [
+                "Register",
+                "Network Storage",
+                "Virtual Memory"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which page replacement algorithm aims to minimize the number of page faults?",
+            "correct_answer": "Optimal",
+            "incorrect_answers": [
+                "FIFO",
+                "LRU",
+                "Random"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which file system structure uses a table of pointers to point to the blocks of a file?",
+            "correct_answer": "Indexed Allocation",
+            "incorrect_answers": [
+                "Contiguous Allocation",
+                "Linked Allocation",
+                "FAT (File Allocation Table)"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Multithreading enables multiple threads to share the same memory space and resources within a process.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False",
+                "IDK",
+                "NON"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "In ____________ scheduling, each process is assigned a fixed time slice to execute before being preempted.",
+            "correct_answer": "Round Robin",
+            "incorrect_answers": [
+                "Bound Bin",
+                "Wound Cin",
+                "Spehrical Robin"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Which methods can be used for inter-process communication (IPC) in operating systems?",
+            "correct_answers": [
+                "Pipes",
+                "Shared Memory",
+                "Message Passing"
+            ],
+            "incorrect_answers": [
+                "Registers",
+                "Stacks",
+                "Queues"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Real-time operating systems are designed to meet specific deadlines for processing tasks in applications such as ____________.",
+            "correct_answer": "Aircraft control systems",
+            "incorrect_answers": [
+                "Word processing",
+                "Web browsing",
+                "Video editing"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "easy",
+            "question": "Which scheduling algorithm guarantees that every process gets a fair share of the CPU time?",
+            "correct_answer": "Round Robin",
+            "incorrect_answers": [
+                "First-Come, First-Served",
+                "Shortest Job Next",
+                "Priority Scheduling"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Thrashing occurs when the system spends more time swapping data between main memory and disk than executing processes.",
+            "correct_answer": "True",
+            "incorrect_answers": [
+                "False",
+                "IDK",
+                "NON"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "In a Unix-like operating system, a new process is created using the ________ system call.",
+            "correct_answer": "fork",
+            "incorrect_answers": [
+                "Hard Disk",
+                "Main Memory",
+                "Network Interface Card"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "Which of the following are common file attributes in most operating systems?",
+            "correct_answers": [
+                "Read",
+                "Write",
+                "Execute"
+            ],
+            "incorrect_answers": [
+                "Compress",
+                "Encrypt",
+                "Archive"
+            ]
+        },
+        {
+            "category": "Operating Systems",
+            "type": "multiple",
+            "difficulty": "hard",
+            "question": "Cache memory is located closest to which component in a computer system?",
+            "correct_answer": "CPU",
+            "incorrect_answers": [
+                "Hard Disk",
+                "Main Memory",
+                "Network Interface Card"
+            ]
+        }
+    ]
+}
 app.get('/quiz', (req, res) => {
-    const filePath = "questions.json";
-    fs.readFile(filePath, 'utf-8')
-        .then((data) => {
-            try {
-                const parsedData = JSON.parse(data);
-                res.json(parsedData);
-            } catch (parseError) {
-                console.error("Error parsing JSON:", parseError);
-                res.status(500).send("Internal Server Error");
-            }
-        })
-        .catch((readError) => {
-            console.error("Error reading JSON file:", readError);
-            res.status(500).send("Internal Server Error");
-        });
+    res.json(data);
+    // const filePath = "questions.json";
+    // fs.readFile(filePath, 'utf-8')
+    //     .then((data) => {
+    //         try {
+    //             const parsedData = JSON.parse(data);
+    //             res.json(parsedData);
+    //         } catch (parseError) {
+    //             console.error("Error parsing JSON:", parseError);
+    //             res.status(500).send("Internal Server Error");
+    //         }
+    //     })
+    //     .catch((readError) => {
+    //         console.error("Error reading JSON file:", readError);
+    //         res.status(500).send("Internal Server Error");
+    //     });
 });
 
 // Default route for other requests
