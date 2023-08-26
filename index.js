a25840bb2920d7ba28cd3f17498ec6c28e4a16db
shubham <shubham@shubhams-MacBook-Air.local>
2023-08-27 01:51:37 +0530

    errr

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-import fs from "fs";
-import express from "express"; 
-import cors from "cors";
-import env from "dotenv"; 
-import {Configuration,OpenAIApi} from "openai"
-env.config();
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-    const filePath = "questions.json";
-    fs.promises.readFile(filePath, 'utf-8').then((data) => {
-        const parsedData = JSON.parse(data);
-        res.json(parsedData); // Use res.json() here
-    }).catch((error) => {
-        console.error("Error reading JSON file:", error);
-        res.status(500).send("Internal Server Error");
-    });
-});
-
-const port = process.env.PORT || 3000; // Set the port correctly
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const express=require("express")
+const app=express();
+const port=process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+app.listen(port,()=>{
+    console.log("Server is running on port 3000");
+})
